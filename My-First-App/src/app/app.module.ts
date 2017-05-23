22a7f5fa71dd6f048e9752ee59db27dd52924241
Mehul Rana <mehul.r@hcopl.com>
2017-05-23 15:57:42 +0530

    #1 Initialization

---
+++ My-First-App/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { serverComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    serverComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
